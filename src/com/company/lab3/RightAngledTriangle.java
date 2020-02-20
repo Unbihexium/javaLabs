dacf6e0743c0ec1e4a0e2a12089d84a68db88eeb
Egor Nalobin <ednalobin@infodev.ru>
2020-02-20 14:56:16 +1000

    доделал класс прямоугольный треугольник

--- src/com/company/lab3/RightAngledTriangle.java
+++ src/com/company/lab3/RightAngledTriangle.java
@@ -4,7 +4,22 @@ import com.company.lab3.Triangle;
 
 public class RightAngledTriangle extends Triangle {
 
+    private double square;
+
     public RightAngledTriangle(double a, double b, double c) {
         super(a, b, c);
+        if (a * a + b * b != c * c)
+            throw new RuntimeException("Треугольник не прямоугольный");
+
     }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public double calcSquare(){
+        this.square =  this.a * this.b / 2.0;
+        return this.square;
+    }
+
 }
