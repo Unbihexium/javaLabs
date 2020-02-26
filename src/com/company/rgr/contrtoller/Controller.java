1707529b29175c06669f622e3391f3503953b97b
Egor Nalobin <ednalobin@infodev.ru>
2020-02-26 18:11:28 +1000

    сделал редактирование самолета по двойному клику

--- src/com/company/rgr/contrtoller/Controller.java
+++ src/com/company/rgr/contrtoller/Controller.java
@@ -6,6 +6,7 @@ import com.company.rgr.utils.Logger;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,10 +14,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
+import javafx.event.EventHandler;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -30,6 +33,8 @@ public class Controller {
     ListView<Plane> planesListView;
 
     private Callback<ListView<Plane>, ListCell<Plane>> callback;
+    private EventHandler<MouseEvent> doubleClick;
+
     public Controller(){
         callback = new Callback<ListView<Plane>, ListCell<Plane>>() {
             @Override
@@ -48,11 +53,48 @@ public class Controller {
                 return cell;
             }
         };
+
+        doubleClick = new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getClickCount() == 2) {
+                    Plane item = null;
+                    item = planesListView.getSelectionModel()
+                            .getSelectedItem();
+                    if (item != null) {
+                        try {
+                            startNewWindow(item);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
     }
 
     public void mouseClicked(){
         logger.log("Tag", "Message");
         System.out.println(planes.size());
+    }
+
+    public void startNewWindow(Plane plane) throws IOException {
+        // Старт нового окна для редактирования
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(getClass().getResource("../view/AddEditPlane.fxml"));
+        Parent root = loader.load();
+        Stage newWindow = new Stage();
+        AddEditPlane controller = loader.getController();
+        controller.parent = this;
+        controller.plane = plane;
+        controller.prepare();
+        newWindow.setTitle("Second Stage");
+        newWindow.setX(0);
+        newWindow.setY(0);
+        newWindow.setScene(new Scene(root, 350, 350));
+        newWindow.setResizable(false);
+        newWindow.show();
     }
 
     public void startNewWindow() throws IOException {
@@ -62,8 +104,9 @@ public class Controller {
         loader.setLocation(getClass().getResource("../view/AddEditPlane.fxml"));
         Parent root = loader.load();
         Stage newWindow = new Stage();
-        ((AddEditPlane)loader.getController()).parent = this;
-        ((AddEditPlane)loader.getController()).prepare();
+        AddEditPlane controller = loader.getController();
+        controller.parent = this;
+        controller.prepare();
         newWindow.setTitle("Second Stage");
         newWindow.setX(0);
         newWindow.setY(0);
@@ -87,6 +130,7 @@ public class Controller {
         ObservableList<Plane> observableList = FXCollections.observableArrayList(planes);
         planesListView.setItems(observableList);
         planesListView.setCellFactory(callback);
+        planesListView.setOnMouseClicked(doubleClick);
     }
 
 }
